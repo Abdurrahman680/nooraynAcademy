b1bbcb27d710168ea9ceeb78ed1bd31d9752ba9f
Abdurrahman680 <muhammadabdurrahmana70@gmail.com>
2026-01-10 06:53:02 +0500

    Remove Noorayn Academy Scholars section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Teachers from './components/Teachers';
-import Scholars from './components/Scholars';
 import Courses from './components/Courses';
 import Fees from './components/Fees';
 import AdmissionForm from './components/AdmissionForm';
@@ -17,7 +16,6 @@ function App() {
       <Hero />
       <About />
       <Teachers />
-      <Scholars />
       <Courses />
       <Fees />
       <AdmissionForm />
